74803e9a19ab6d78bb5c8526a714a41da144bfce
anuja.a <anuja.a@ergobite.com>
2023-07-25 09:42:31 +0530

    node js socket

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
+const publicPath = path.join(__dirname, "/../public");
+const port = 5452;
+var app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on("connection", (socket) => {
+  console.log("A new user just connected");
+
+  socket.on("disconnect", () => {
+    console.log("User was disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
